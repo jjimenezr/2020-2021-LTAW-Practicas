05ce4d3d44383eb97ef6f3235009619ab06efafc
jjimenezr <j.jimenezr.2016@alumnos.urjc.es>
2021-03-09 12:36:55 +0100

    comenzando por la pagina de error 404

--- P1/Tienda.js
+++ P1/Tienda.js
@@ -15,23 +15,40 @@ const server = http.createServer((req, res) => {
   let client_request = url.parse(req.url, true);
   let file = "";
   let Type = "";
-  console.log(client_request);
+  //console.log(client_request.pathname);
 
   //-- dependiendo del pathname le damos una u otra cosa
   if (client_request.pathname == '/favicon.ico') {
     file = './imagenes_tienda/icon.png';
-    type = 'image/png'
   }else if (client_request.pathname == '/') {
     file = './home.html';
-    type = 'text/html'
+  }else{
+    file = "." + client_request.pathname;
   }
 
   //-- leemos el rescurso path para saber que entregarle al cliente
   fs.readFile(file, (err, data) => {
+    //-- clasificamos que tipo de archivo me pide
+    if (file.split('.')[2] == 'css') {
+      console.log(file.split('.')[2])
+      type = 'text/css';
+    }else if (file.split('.')[2] == 'gif') {
+      console.log(file.split('.')[2])
+      type = 'image/gif';
+    }else if (file.split('.')[2] == 'html') {
+      console.log(file.split('.')[2])
+      type = 'text/html';
+    }else if (file.split('.')[2] == 'png') {
+      console.log(file.split('.')[2])
+      type = 'image/png';
+    }
 
     if (err) {  //-- Ha ocurrido algun error
-      console.log("Error!!")
-      console.log(err.message);
+      res.statusCode = 404;   //-- codigo para decir que no se ha encontrado el archivo
+      res.statusMessage = "NOT FOUND";   //-- mensaje de que no se ha encontrado el archivo
+      res.setHeader('Content-Type', 'text/html');
+      res.write(fs.readFileSync('./404error.html'));
+      res.end();
     } else {
       //-- terminamos la respuestas
       res.statusCode = 200;   //-- codigo para decir que todo va bien

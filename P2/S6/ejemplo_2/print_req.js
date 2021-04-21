839790fe53a784bb9e03da36efe1b49554bb6980
jjimenezr <j.jimenezr.2016@alumnos.urjc.es>
2021-04-21 19:17:05 +0200

    ejemplo 2 realizado

---
+++ P2/S6/ejemplo_2/print_req.js
@@ -0,0 +1,45 @@
+//-- Imprimir información sobre la solicitud recibida
+
+const http = require('http');
+const fs = require('fs');
+const PUERTO = 9000;
+
+//-- SERVIDOR: Bucle principal de atención a clientes
+const server = http.createServer((req, res) => {
+
+  //-- Construir el objeto url con la url de la solicitud
+  const myURL = new URL(req.url, 'http://' + req.headers['host']);  
+  console.log("");
+  console.log("Método: " + req.method);
+  console.log("Recurso: " + req.url);
+  console.log("  Ruta: " + myURL.pathname);
+  console.log("  Parametros: " + myURL.searchParams);
+
+  //-- Por defecto entregar formulario
+  let content = fs.readFileSync('form2.html','utf-8');;
+
+  if (myURL.pathname == '/procesar') {
+      content = fs.readFileSync('form1_resp.html', 'utf-8');
+  }
+
+  //-- Si hay datos en el cuerpo, se imprimen
+  req.on('data', (cuerpo) => {
+
+    //-- Los datos del cuerpo son caracteres
+    req.setEncoding('utf8');
+    console.log(`Cuerpo (${cuerpo.length} bytes)`)
+    console.log(` ${cuerpo}`);
+  });
+
+  //-- Esto solo se ejecuta cuando llega el final del mensaje de solicitud
+  req.on('end', ()=> {
+    //-- Generar respuesta
+    res.setHeader('Content-Type', "text/html");
+    res.write(content);
+    res.end()
+  });
+
+});
+
+server.listen(PUERTO);
+console.log("Escuchando en puerto: " + PUERTO);

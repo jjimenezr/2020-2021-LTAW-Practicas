e2203871204f0ffac46e7b6b77c3cd76b3c86731
jjimenezr <j.jimenezr.2016@alumnos.urjc.es>
2021-05-12 16:26:41 +0200

    version 3 ventana

--- P4/S10/main.js
+++ P4/S10/main.js
@@ -6,5 +6,12 @@ console.log("Arrancando proton digo electron...");
 //-- Punto de entrada. En cuanto electron está listo,
 //-- ejecuta esta función
 electron.app.on('ready', ()=>{
-    console.log("Evento Ready for the battle!")
+    console.log("Evento Ready for the battle!");
+
+    //-- Crear la ventana principal de nuestra aplicación
+    win = new electron.BrowserWindow({
+        width: 800,  //-- Anchura 
+        height: 600  //-- Altura
+    });
+
 });

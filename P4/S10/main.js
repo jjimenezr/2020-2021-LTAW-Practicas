4c886e2caada8e3b69fbe841f4cb7f32a9372daa
jjimenezr <j.jimenezr.2016@alumnos.urjc.es>
2021-05-12 16:28:11 +0200

    version 4 sin barra de menu

--- P4/S10/main.js
+++ P4/S10/main.js
@@ -14,4 +14,9 @@ electron.app.on('ready', ()=>{
         height: 600  //-- Altura
     });
 
+    //-- En la parte superior se nos ha creado el menu
+    //-- por defecto
+    //-- Si lo queremos quitar, hay que añadir esta línea
+    win.setMenuBarVisibility(false);
+
 });

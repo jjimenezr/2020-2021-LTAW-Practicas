3bbb7526f4b46e3e2cd051da942ec40ba0c70ba8
jjimenezr <j.jimenezr.2016@alumnos.urjc.es>
2021-05-12 16:31:30 +0200

    version 5 cargando pagina externa

--- P4/S10/main.js
+++ P4/S10/main.js
@@ -17,6 +17,10 @@ electron.app.on('ready', ()=>{
     //-- En la parte superior se nos ha creado el menu
     //-- por defecto
     //-- Si lo queremos quitar, hay que añadir esta línea
-    win.setMenuBarVisibility(false);
+    // win.setMenuBarVisibility(false);
+
+    //-- Cargar contenido web en la ventana
+    //-- La ventana es en realidad.... ¡un navegador!
+    win.loadURL('https://www.yugioh-card.com/es/');
 
 });
